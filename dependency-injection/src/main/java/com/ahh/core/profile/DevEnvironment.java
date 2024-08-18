ee918fb5ae707b690b96013c15aed2bf16b867a2
adrianhh1426 <adrianhh1426@gmail.com>
2024-08-17 19:13:00 -0600

    creando profiles

---
+++ dependency-injection/src/main/java/com/ahh/core/profile/DevEnvironment.java
@@ -0,0 +1,15 @@
+package com.ahh.core.profile;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("dev")
+public class DevEnvironment implements EnvironmentService{
+
+	@Override
+	public String getEnvironment() {
+		return "Dev";
+	}
+
+}
---
+++ dependency-injection/src/main/java/com/ahh/core/profile/EnvironmentService.java
@@ -0,0 +1,5 @@
+package com.ahh.core.profile;
+
+public interface EnvironmentService {
+	public String getEnvironment();
+}
