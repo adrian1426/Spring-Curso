287bc742e4c76663e5ecbd2aae7bc6c98f9444f2
adrianhh1426 <adrianhh1426@gmail.com>
2024-08-17 21:45:21 -0600

    Bean explicito

--- dependency-injection/src/main/java/com/ahh/core/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/ahh/core/DependencyInjectionApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import com.ahh.core.scopes.EjemploScopeService;
 
@@ -13,6 +14,11 @@ import com.ahh.core.scopes.EjemploScopeService;
 public class DependencyInjectionApplication {
 	
 	public static final Logger log = LoggerFactory.getLogger(DependencyInjectionApplication.class);
+	
+	@Bean
+	public String getAppName() {
+		return "my app name is App";
+	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
@@ -22,6 +28,9 @@ public class DependencyInjectionApplication {
 		
 		log.info("Bean equals {}",ejemploScopeService.equals(ejemploScopeService2));
 		log.info("Bean == {}",ejemploScopeService==ejemploScopeService2);
+		
+		String nombreApp =context.getBean(String.class);
+		log.info("Nombre app {}",nombreApp);
 
 	}
 
